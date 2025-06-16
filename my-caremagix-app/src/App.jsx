08c57c2758aff38d30b1c21f36a4fb00a0dd2868
Sushil Gupta <sushil.gupta@infinitylabs.in>
2025-06-16 02:17:00 -0700

    setup reactjs and tailwind

---
+++ my-caremagix-app/src/App.jsx
@@ -0,0 +1,19 @@
+
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      
+      </div>
+      <p className="border">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
